23e84b1f2eccd27ebc31b5cc3e6bad135f9a9e93
grom <takashi-san@yandex.ru>
2022-03-03 18:24:30 +0500

    Подготовка задач для урока 17

---
+++ 17-prog/src/main/java/ru/otus/oop/atm/interfaces/ATM.java
@@ -0,0 +1,11 @@
+package ru.otus.oop.atm.interfaces;
+
+import ru.otus.oop.atm.dictionary.Denomination;
+
+public interface ATM {
+    void withdraw(int amount);
+
+    void depositMoney(Denomination denomination, int amount);
+
+    AccountState accountState();
+}
