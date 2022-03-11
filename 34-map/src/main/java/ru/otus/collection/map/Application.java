6401124c7fd35fc0af6cb08abf9f54332361ae5c
grom <takashi-san@yandex.ru>
2022-03-11 19:57:41 +0500

    Подготовка задач для урока 34

--- 34-map/src/main/java/ru/otus/collection/map/Application.java
+++ 34-map/src/main/java/ru/otus/collection/map/Application.java
@@ -5,9 +5,17 @@ import java.util.Map;
 public class Application {
     public static void main(String[] args) {
         Map<String, String> myMap = new MapImpl<>();
-        myMap.put("key", "value");
-        String value = myMap.get("key");
+        myMap.put("key1", "value1");
+        myMap.put("key2", "value2");
+        myMap.put("key3", "value3");
+        myMap.put("key4", "value4");
+        myMap.put("key5", "value5");
+        myMap.put("key6", "value6");
+        myMap.put("key7", "value7");
 
-        System.out.println(value);
+        for (int i = 0; i < 7; i++) {
+            String val = myMap.get("key" + (i + 1));
+            System.out.println(val);
+        }
     }
 }
--- 34-map/src/main/java/ru/otus/collection/map/MapImpl.java
+++ 34-map/src/main/java/ru/otus/collection/map/MapImpl.java
@@ -1,10 +1,11 @@
 package ru.otus.collection.map;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapImpl<K, V> implements Map<K, V> {
+    private List<V>[] values = new ArrayList[2];
+    private List<K>[] keys = new ArrayList[2];
+    private int loadFactor = 0;
 
     @Override
     public int size() {
@@ -18,11 +19,81 @@ public class MapImpl<K, V> implements Map<K, V> {
 
     @Override
     public V get(Object key) {
+        int hc = key.hashCode();
+        int index = hc % values.length;
+
+        List<K> keyBucket = keys[index];
+        if (keyBucket == null || !keyBucket.contains(key)) {
+            return null;
+        }
+
+        // если ключ есть
+        if (keyBucket.contains(key)) {
+            int keyIndex = keyBucket.indexOf(key);
+            V value = values[index].get(keyIndex);
+            return value;
+        }
+
+
         return null;
     }
 
     @Override
     public V put(K key, V value) {
+        int hc = key.hashCode();
+        int index = hc % values.length;
+
+        List<K> keyBucket = keys[index];
+
+        // если нет хранилища ключей
+        if (keyBucket == null) {
+            List<K> keysBucket = new ArrayList<>();
+            keysBucket.add(key);
+            keys[index] = keysBucket;
+
+            List<V> valuesBucket = new ArrayList<>();
+            valuesBucket.add(value);
+            values[index] = valuesBucket;
+            calcLoadFactor();
+            return value;
+        }
+
+        // если нет ключа
+        if (!keyBucket.contains(key)) {
+            keyBucket.add(key);
+            values[index].add(value);
+            calcLoadFactor();
+            return value;
+        }
+
+        // если есть и ключ и значение
+        if (keyBucket.contains(key)) {
+            int keyBucketIndex = keyBucket.indexOf(key);
+            List<V> bucket = values[index];
+            if (bucket.contains(value) && bucket.indexOf(value) == keyBucketIndex) {
+                return value;
+            }
+        }
+
+        // если ключ есть а значение другое
+        if (keyBucket.contains(key)) {
+            List<V> bucket = values[index];
+            int keyBucketIndex = keyBucket.indexOf(key);
+            if (!bucket.contains(value)) {
+                List<V> temp = new ArrayList<>();
+                for (int i = 0; i < bucket.size(); i++) {
+                    if (i != keyBucketIndex) {
+                        temp.add(bucket.get(i));
+                    } else {
+                        temp.add(value);
+                    }
+                }
+                values[index] = temp;
+                calcLoadFactor();
+                return value;
+            }
+        }
+
         return null;
     }
 
@@ -34,6 +105,90 @@ public class MapImpl<K, V> implements Map<K, V> {
     @Override
     public void clear() {
 
+    }
+
+    private void resize() {
+        if (loadFactor >= 3) {
+            List<V>[] tempValues = new ArrayList[keys.length * 2];
+            List<K>[] tempKeys = new ArrayList[keys.length * 2];
+
+            for (int i = 0; i < keys.length; i++) {
+                List<K> keyBucket = keys[i];
+                List<V> valueBucket = values[i];
+                for (int j = 0; j < keyBucket.size(); j++) {
+                    K key = keyBucket.get(j);
+                    V value = valueBucket.get(j);
+                    resizedPut(key, value, tempValues, tempKeys);
+                }
+            }
+
+            values = tempValues;
+            keys = tempKeys;
+            loadFactor = 0;
+            calcLoadFactor();
+        }
+    }
+
+    private void resizedPut(K key, V value, List<V>[] tempValues, List<K>[] tempKeys) {
+        int hc = key.hashCode();
+        int index = hc % tempValues.length;
+
+        List<K> keyBucket = tempKeys[index];
+
+        // если нет хранилища ключей
+        if (keyBucket == null) {
+            List<K> keysBucket = new ArrayList<>();
+            keysBucket.add(key);
+            tempKeys[index] = keysBucket;
+
+            List<V> valuesBucket = new ArrayList<>();
+            valuesBucket.add(value);
+            tempValues[index] = valuesBucket;
+            return;
+        }
+
+        // если нет ключа
+        if (!keyBucket.contains(key)) {
+            keyBucket.add(key);
+            tempValues[index].add(value);
+            return;
+        }
+
+        // если есть и ключ и значение
+        if (keyBucket.contains(key)) {
+            int keyBucketIndex = keyBucket.indexOf(key);
+            List<V> bucket = tempValues[index];
+            if (bucket.contains(value) && bucket.indexOf(value) == keyBucketIndex) {
+                return;
+            }
+        }
+
+        // если ключ есть а значение другое
+        if (keyBucket.contains(key)) {
+            List<V> bucket = tempValues[index];
+            int keyBucketIndex = keyBucket.indexOf(key);
+            if (!bucket.contains(value)) {
+                List<V> temp = new ArrayList<>();
+                for (int i = 0; i < bucket.size(); i++) {
+                    if (i != keyBucketIndex) {
+                        temp.add(bucket.get(i));
+                    } else {
+                        temp.add(value);
+                    }
+                }
+                tempValues[index] = temp;
+                return;
+            }
+        }
+    }
+
+    private void calcLoadFactor() {
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] != null && values[i].size() > loadFactor) {
+                loadFactor = values[i].size();
+            }
+        }
+        resize();
     }
 
     /**
